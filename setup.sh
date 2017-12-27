c4a4435b292882bedda4c9d5841229212bc5c61a
Christopher EnyTC <chris@enytc.com>
2017-12-27 03:07:55 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ echo "Making Zsh the default shell"
 chsh -s $(which zsh)
 
 echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 
 # Install utilities
 
